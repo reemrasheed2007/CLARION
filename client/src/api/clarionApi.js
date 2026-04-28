f71514d295698def8ce19f675f3d4a6cadf198d2
reemrasheed2007 <reemrasheed2007@gmail.com>
2026-04-28 23:29:59 +0530

    update base url for production

--- client/src/api/clarionApi.js
+++ client/src/api/clarionApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://clarion-backend-yags.onrender.com/api',
   timeout: 60000,
 });
 
